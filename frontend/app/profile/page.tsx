5a721a0b833930868787fea63bf718a44313137e
aromi ben <158552866+benardaromi@users.noreply.github.com>
2024-08-11 13:44:21 +0000

    completing profile side bar

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -2,8 +2,8 @@
 
 import { useAuth } from "@/components/Auth"
 import Loading from "@/components/Loading"
-import { Avatar } from "@mui/material"
-import { useRouter } from "next/navigation"
+import { Input } from "@mui/material"
+import { BellIcon, EnvelopeClosedIcon, RowsIcon } from "@radix-ui/react-icons"
 
 export default function Profile() {
     const {user, userId, userName, photoURL, loading} = useAuth()
@@ -13,8 +13,19 @@ export default function Profile() {
     }
 
     return (
-        <div className="h-full w-full bg-red-400">
-            im the main page
+        <div className="h-full w-full bg-slate-50 p-10">
+            <div className="flex justify-around items-center bg-white drop-shadow p-1 rounded">
+                <div className="flex space-x-6">
+                    <EnvelopeClosedIcon className="hover:cursor-pointer hover:scale-110 transition hover:text-sky-400"/>
+                    <BellIcon className="hover:cursor-pointer hover:scale-110 transition hover:text-sky-400 z-50"/>
+                </div>
+                <div className="flex-shrink-0 fle">
+                    <Input placeholder={`search account`}/>
+                </div>
+                <div>
+                    <RowsIcon className="hover:cursor-pointer hover:scale-110 transition hover:text-sky-400"/>
+                </div>
+            </div>
         </div>
     )
 }
